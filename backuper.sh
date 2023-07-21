19e214115bff641496e1d7ece37fcfa1f97640e6
will.t <will.t@unimus.net>
2023-07-21 11:20:15 +0200

    first commit

---
+++ backuper.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+UNIMUS_ADDRESS="172.17.0.1:8085"
+TOKEN="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhdXRoMCJ9.Ko3FEfroI2hwNT-8M-8Us38gqwzmHHxypM7nWCqU2JA"
+HEADERS_ACCEPT="Accept: application/json"
+HEADERS_CONTENT_TYPE="Content-type: application/json"
+HEADERS_AUTHORIZATION="Authorization: Bearer $TOKEN"
+
+#ftp root directory
+FTP_FOLDER="/home/will/docker-composer/ftp_data/"
+
+#variable for enabling(1) creation of new devices in Unimus
+CREATE_DEVICES=1
+
+process_files() {
+    local directory="$1"
+    for subdir in "$directory"/*; do
+        if [ -d "$subdir" ]; then
+            address=$(basename "$subdir")
+            id=$(get_device_id "$address")
+            [ $id = "null" ] && [ $CREATE_DEVICES = 1 ] && create_new_device "$address" && id=$(get_device_id "$address")
+            for file in $(ls -tr "$subdir"); do
+                echo -e "\nCurrent file: " $file
+                if [ -f "$subdir/$file" ]; then
+                    encoded_backup=$(base64 -w 0 "$subdir/$file")
+                    isTextFile=$(file -b "$subdir/$file")
+                    if [[ $isTextFile == *"text"* ]]; then
+                        create_backup "$id" "$encoded_backup" "TEXT" && echo -e "created TEXT backup\n" && rm "$subdir/$file"
+                        sleep 1
+                    else
+                        create_backup "$id" "$encoded_backup" "BINARY" && echo -e "created BINARY backup\n" && rm "$subdir/$file"
+                        sleep 1
+                    fi
+                fi
+            done
+        fi
+    done
+}
+
+create_new_device() {
+curl -sSL -H "$HEADERS_ACCEPT" -H "$HEADERS_CONTENT_TYPE" -H "$HEADERS_AUTHORIZATION" -d '{"address": "'"$1"'","description":"apicreated"}'\
+ "http://$UNIMUS_ADDRESS/api/v2/devices" > /dev/null
+}
+
+get_device_id() {
+echo "$(curl -sSL -H "$HEADERS_ACCEPT" -H "$HEADERS_AUTHORIZATION" "http://$UNIMUS_ADDRESS/api/v2/devices/findByAddress/$1" | jq .data.id)"
+}
+
+create_backup() {
+curl -sSL -H "$HEADERS_ACCEPT" -H "$HEADERS_CONTENT_TYPE" -H "$HEADERS_AUTHORIZATION" -d '{"backup": "'"$2"'","type":"'"$3"'"}' "http://$UNIMUS_ADDRESS/api/v2/devices/$1/backups" > /dev/null
+}
+
+process_files $FTP_FOLDER
---
+++ unimus_backup.ps1
@@ -0,0 +1,115 @@
+#PowerShell script that extracts the names of subdirectories within a specified directory:
+$UNIMUS_ADDRESS = "172.17.0.1:8085"
+$TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhdXRoMCJ9.Ko3FEfroI2hwNT-8M-8Us38gqwzmHHxypM7nWCqU2JA"
+
+$FTPFOLDER = "/ftp_data/"
+
+#Variable controlling creation of new devices in Unimus; 1 = create
+$CREATE_DEVICES = 1
+
+function Process-Files {
+    param(
+        [string]$directory
+    )
+
+    $subdirs = Get-ChildItem -Path $directory -Directory
+    foreach ($subdir in $subdirs) {
+        $address = $subdir.Name
+        $id = "null"; $id = Get-DeviceId $address
+        Write-Host "`nDEVICE ID IS: $id"
+
+        if ($id -eq "null" -and $CREATE_DEVICES -eq 1) {
+            Create-NewDevice $address
+            $id = Get-DeviceId $address
+        }
+        #Write-Host "`nDEVICE ID IS: $id"
+        $files = Get-ChildItem -Path $subdir.FullName | Sort-Object -Property LastWriteTime -Descending
+        foreach ($file in $files) {
+            if ($file.GetType() -eq [System.IO.FileInfo]) {
+                Write-Host "Processing file: $($file.Name)"
+                $encodedBackup = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($file.Fullname))
+                $content = Get-Content -Path $file.FullName -Raw
+                if ($content -match "[^\x00-\x7F]") {
+                    Create-Backup $id $encodedBackup "BINARY"
+                    Write-Host "`nCreated BINARY backup"
+                    Remove-Item $file.FullName
+                } else {
+                    Create-Backup $id $encodedBackup "TEXT"
+                    Write-Host "`nCreated TEXT backup"
+                    Remove-Item $file.FullName
+                }
+            }
+        }
+    }
+}
+
+function Create-NewDevice {
+    param(
+        [string]$address
+    )
+
+    $body = @{
+        address = $address
+        description = "apicreated"
+    } | ConvertTo-Json
+
+    $headers = @{
+        "Accept" = "application/json"
+        "Content-Type" = "application/json"
+        "Authorization" = "Bearer $TOKEN"
+    }
+
+    Invoke-RestMethod -Uri "http://$UNIMUS_ADDRESS/api/v2/devices" -Method POST -Headers $headers -Body $body | Out-Null
+}
+
+function Get-DeviceId {
+    param(
+        [string]$address
+    )
+
+    $headers = @{
+        "Accept" = "application/json"
+        "Authorization" = "Bearer $TOKEN"
+    }
+
+#    $response = Invoke-RestMethod -Uri "http://$UNIMUS_ADDRESS/api/v2/devices/findByAddress/$address" -Method GET -Headers $headers
+#    $response.data.id
+
+    try {
+        $response = Invoke-RestMethod -Uri "http://$UNIMUS_ADDRESS/api/v2/devices/findByAddress/$address" -Method GET -Headers $headers
+        return $response.data.id
+    }
+    catch {
+        if ($_.Exception.Response.StatusCode -eq 404) {
+            Write-Host "Device with address $address not found."
+            return "null"
+        }
+        else {
+            Write-Host "An error occurred: $($_.Exception.Message)"
+            return $null
+        }
+    }
+}
+
+function Create-Backup {
+    param(
+        [string]$id,
+        [string]$encodedBackup,
+        [string]$type
+    )
+
+    $body = @{
+        backup = $encodedBackup
+        type = $type
+    } | ConvertTo-Json
+
+    $headers = @{
+        "Accept" = "application/json"
+        "Content-Type" = "application/json"
+        "Authorization" = "Bearer $TOKEN"
+    }
+
+    Invoke-RestMethod -Uri "http://$UNIMUS_ADDRESS/api/v2/devices/$id/backups" -Method POST -Headers $headers -Body $body | Out-Null
+}
+
+Process-Files -directory $FTPFOLDER
